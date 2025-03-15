f7725885b1ded29f5a31bc11688f3dfde0ec597e
sp1r17 <z.chisenhall@gmail.com>
2025-03-15 13:32:50 -0500

    proper scaling

--- Projects/UOContent/Spells/Base/SpellHelper.cs
+++ Projects/UOContent/Spells/Base/SpellHelper.cs
@@ -518,7 +518,7 @@ namespace Server.Spells
             }
 
             //var scale = 1.0 + (caster.Skills.Magery.Value - 100.0) / 200.0;
-            var scale = 0.5 * (caster.Skills.SpiritSpeak.Value / 100.0);
+            var scale = 1.5 * (caster.Skills.SpiritSpeak.Value / 100.0);
 
             if (scaleDuration)
             {
